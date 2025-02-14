ffafa9048b4e8366e5bc69171028456b746693d5
RachelNGS <rshepp261@gmail.com>
2025-02-14 21:47:33 +0000

    Vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Loader from '@/components/loader';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function Home() {
   return (
     <main className="min-h-screen p-8 bg-gray-50">
       <Loader />
+      <Analytics />
     </main>
   );
 }
